5edd9be705c641fa3135690169eade750545dbf4
Eric Powell <pow13009@byui.edu>
2019-01-17 10:34:28 -0700

    Active css activate 2 Electric Boogaloo

--- web/Teach02/header.php
+++ web/Teach02/header.php
@@ -1,9 +1,7 @@
 <?php 
 echo "<h1>Not Clearly Enough</h1>";
-if ($page = "home")
+
 	echo "<a href='home.php'>Home</a>";
-elseif ($page = "about-us") 
 	echo "<a href='about-us.php'>About Us</a>";
-else
 	echo "<a href='login.php'>Login</a>";
 ?>
