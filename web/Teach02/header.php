7ed8fe1ef97a3586bc364079a3f2e03fcc5174c0
Eric Powell <pow13009@byui.edu>
2019-01-17 10:25:39 -0700

    semicolons

--- web/Teach02/header.php
+++ web/Teach02/header.php
@@ -1,6 +1,6 @@
 <?php 
-echo "<h1>Not Clearly Enough</h1>"
-echo "<a href='home.php'>Home</a>"
-echo "<a href='about-us.php'>About Us</a>"
-echo "<a href='login.php'>Login</a>"
+echo "<h1>Not Clearly Enough</h1>";
+echo "<a href='home.php'>Home</a>";
+echo "<a href='about-us.php'>About Us</a>";
+echo "<a href='login.php'>Login</a>";
 ?>
