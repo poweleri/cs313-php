e269614ea767e36efe58117976c5d36afd8fbc17
Eric Powell <pow13009@byui.edu>
2019-01-17 11:27:32 -0700

    Still stupid 2

--- web/Teach02/signin.php
+++ web/Teach02/signin.php
@@ -3,9 +3,11 @@ session_start();
 ?>
 
 <?php
+	session_unset()
+	
 	if ($_GET["user"] = admin)
 		$_SESSION["user"] = admin;
-	else
+	elseif($_GET["user"] = tester)
 		$_SESSION["user"] = tester;
 
 	header('location: /Teach02/home.php');
