f8e2d72a2e2a1a7be9197d9fd2194a64ed1be8b4
Eric Powell <pow13009@byui.edu>
2019-01-17 10:52:47 -0700

    Final step for essential steps

--- web/Teach02/header.php
+++ web/Teach02/header.php
@@ -1,21 +1,20 @@
 <div>
 	<?php
-		echo "$page"; 
 		echo "<h1>Not Clearly Enough</h1>";
 		if($page == "/Teach02/home.php") {
-			echo "<a href='home.php' style=\"text-weight: bold;\">Home</a>
+			echo "<a href='home.php' style=\"background-color: yellow;\">Home</a>
 			      <a href='about-us.php'>About Us</a>
 			      <a href='login.php'>Login</a>";
 		}
 		elseif($page == "/Teach02/about-us.php") {
 			echo "<a href='home.php'>Home</a>
-			      <a href='about-us.php' style=\"text-weight: bold;\">About Us</a>
+			      <a href='about-us.php' style=\"background-color: yellow;\">About Us</a>
 			      <a href='login.php'>Login</a>";
 		}
 		else {
 			echo "<a href='home.php'>Home</a>
 			      <a href='about-us.php'>About Us</a>
-			      <a href='login.php' style=\"text-weight: bold;\">Login</a>";
+			      <a href='login.php' style=\"background-color: yellow;\">Login</a>";
 		}
 	?>
 </div>
