f0eada88888df68f8f26717aa53e61d705332306
Eric Powell <pow13009@byui.edu>
2019-01-19 23:41:39 -0700

    Again!

--- web/header.php
+++ web/header.php
@@ -1,22 +1,22 @@
 <div class="container-fluid">
+	<br/>
 	<div class="row">
 		<?php
-		    echo "<br/>";
 			if ($page == '/about-me.php') {
-				echo "<a class=\"col\" href\"/index.php\">Homepage</a>
+				echo "<a class=\"col\" href=\"/index.php\">Homepage</a>
 				      <h3 class=\"col\">About Me</h3>
-				      <a class=\"col\" href\"/assignments.php\">Assignments</a>";
+				      <a class=\"col\" href=\"/assignments.php\">Assignments</a>";
 				      
 			} elseif ($page == '/assignments.php'){
-				echo "<a class=\"col\" href\"/index.php\">Homepage</a>
-				      <a class=\"col\" href\"/about-me.php\">About Me</a>
+				echo "<a class=\"col\" href=\"/index.php\">Homepage</a>
+				      <a class=\"col\" href=\"/about-me.php\">About Me</a>
 				      <h3 class=\"col\">Assignments</h3>";
 			} else {
 				echo "<h3 class=\"col\">Homepage</h3>
-				      <a class=\"col\" href\"/about-me.php\">About Me</a>
-				      <a class=\"col\" href\"/assignments.php\">Assignments</a>";
+				      <a class=\"col\" href=\"/about-me.php\">About Me</a>
+				      <a class=\"col\" href=\"/assignments.php\">Assignments</a>";
 			}
-		    echo "<br/>";
 		?>
 	</div>
+	<br/>
 </div>
