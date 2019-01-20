385046a2502954b1dca49060ade548829a87a508
Eric Powell <pow13009@byui.edu>
2019-01-19 23:33:31 -0700

    Working on a few things to make things look nicer

--- web/header.php
+++ web/header.php
@@ -1,20 +1,19 @@
 <div class="container-fluid">
 	<div class="row">
 		<?php
-			echo "<p>$page</p>";
-			if ($page == 'about-me.php') {
-				echo "<a class=\"col\" href\"index.php\">Homepage</a>
+			if ($page == '/about-me.php') {
+				echo "<a class=\"col\" href\"/index.php\">Homepage</a>
 				      <h3 class=\"col\">About Me</h3>
-				      <a class=\"col\" href\"assignments.php\">Assignments</a>";
+				      <a class=\"col\" href\"/assignments.php\">Assignments</a>";
 				      
-			} elseif ($page == 'assignments.php'){
-				echo "<a class=\"col\" href\"index.php\">Homepage</a>
-				      <a class=\"col\" href\"about-me.php\">About Me</a>
+			} elseif ($page == '/assignments.php'){
+				echo "<a class=\"col\" href\"/index.php\">Homepage</a>
+				      <a class=\"col\" href\"/about-me.php\">About Me</a>
 				      <h3 class=\"col\">Assignments</h3>";
 			} else {
 				echo "<h3 class=\"col\">Homepage</h3>
-				      <a class=\"col\" href\"about-me.php\">About Me</a>
-				      <a class=\"col\" href\"assignments.php\">Assignments</a>";
+				      <a class=\"col\" href\"/about-me.php\">About Me</a>
+				      <a class=\"col\" href\"/assignments.php\">Assignments</a>";
 			}
 
 		?>
