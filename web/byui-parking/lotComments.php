2fc18cd687cd91d1ea995a084213ade6219a0c0e
Eric Powell <pow13009@byui.edu>
2019-02-26 00:22:34 -0700

    stupid ;

--- web/byui-parking/lotComments.php
+++ web/byui-parking/lotComments.php
@@ -32,10 +32,9 @@
 		<div>
 			<h2>User Ratings</h2>
 			<?php
-				$noteQuery = "SELECT lc.lot_comment_info as note, lc.rating, u.username FROM lot_comment lc INNER JOIN usr u ON lc.usr_id=u.usr_id AND lc.parking_lot_id = :lot";
-				$statement = $db->prepare($query);
-				$statement->bindValue(':lot', $lot_id, PDO::PARAM_INT);
-				$statement->execute();
+				$noteQuery = "SELECT lc.lot_comment_info as note, lc.rating, u.username 
+							  FROM lot_comment lc INNER JOIN usr u ON lc.usr_id=u.usr_id AND lc.parking_lot_id = $lot_id;";
+				$statement = $db->query($query);
 				while($row = $statement->fetch(PDO::FETCH_ASSOC)){
 					$note = $row['note'];
 					$rating = $row['rating'];
